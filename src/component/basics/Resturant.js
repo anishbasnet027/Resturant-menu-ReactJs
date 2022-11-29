57a78adaa69a7f0914e0be5643f910ccf8c905b8
Anish Basnet <anishbasnet027@gmail.com>
2022-11-29 14:39:42 +0545

    props and hooks

--- src/component/basics/Resturant.js
+++ src/component/basics/Resturant.js
@@ -1,32 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./Style.css"
+import Menu from "./menuApp.js"
+import Menucard from './Menucard'
 
 const Resturant = () => {
+ const [menuData, setMenuData] = useState(Menu);
+
   return (
     <>
-    <div className="card-container">
-        <div className="card">
-            <div className="card-body">
-                <span className='card-number card-circle subtle'>
-                    1
-                </span>
-                <span className='card-author subtle'>
-                  Breakfast
-                </span>
-                <h2 className='card-title'>Maggi</h2>
-                <span className='card-description subtle'>
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id neque magnam rerum corporis error natus molestias repellat vero. Sunt laudantium doloribus, accusantium assumenda ratione ipsum dolore debitis aliquid officia magni?
-                </span>
-                <div className="card-read">
-                  Read
-                </div>
-            </div>
-            <img src={images} alt="images" className='card-media'/>
-            <span className='card-tag subtle'>
-              Order Now
-            </span>
-        </div>
-    </div>
+    <Menucard mData={menuData}/>
     </>
   )
 }
