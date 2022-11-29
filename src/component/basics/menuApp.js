ca528c6b53be30067c3b0b38110bd6d1255e24ed
Anish Basnet <anishbasnet027@gmail.com>
2022-11-29 14:19:14 +0545

    front page:

---
+++ src/component/basics/menuApp.js
@@ -0,0 +1,41 @@
+const Menu = [
+    {
+    id:1,
+    image:"",
+    name:"Maggi",
+    category:"lunch",
+    price:101,
+    description:"loremamd dafmewfa mefmwafmm mmamfma mfmeamm emafm"
+    },
+    {
+        id:1,
+        image:"",
+        name:"Maggi",
+        category:"lunch",
+        price:101,
+        description:"loremamd dafmewfa mefmwafmm mmamfma mfmeamm emafm"
+        },
+    
+        {
+            id:1,
+            image:"",
+            name:"Maggi",
+            category:"lunch",
+            price:101,
+            description:"loremamd dafmewfa mefmwafmm mmamfma mfmeamm emafm"
+            },
+    
+            {
+                id:1,
+                image:"",
+                name:"Maggi",
+                category:"lunch",
+                price:101,
+                description:"loremamd dafmewfa mefmwafmm mmamfma mfmeamm emafm"
+                },
+                
+
+];
+
+
+export default Menu;
